3347197afd4256763f208dc6120692e67a3bbc11
Evan Steitz <ejsteitz@gmail.com>
2017-10-10 12:28:39 -0600

    volume mount tls

--- controller.sh
+++ controller.sh
@@ -24,13 +24,13 @@ if [ $1 = "apply" ]; then
   # TODO Handle erroneous or missing inputs
   # Bring up a new armada container with passed in yaml mounted to the container
   echo "Creating an Armada container..."
-  docker run -d --net host -p 8000:8000 --name armada  -v $(readlink $(dirname $2)):$(readlink $(dirname $2)) -v ~/.kube/config:/armada/.kube/config -v ~/.kube/plugins/armada/examples/:/examples quay.io/attcomdev/armada:latest
+  docker run -d --net host -p 8000:8000 --name armada  -v $(readlink $(dirname $2)):$(readlink $(dirname $2)) -v ~/.bootkube/tls:/.bootkube/tls -v ~/.kube/config:/armada/.kube/config -v ~/.kube/plugins/armada/examples/:/examples quay.io/attcomdev/armada:latest
   docker exec armada armada apply $(readlink $2)
 else
   # For any other command the chart does not need to be mounted to the container
   # Bring up a new armada container
   echo "Creating an Armada container..."
-  docker run -d --net host -p 8000:8000 --name armada -v ~/.kube/config:/armada/.kube/config -v ~/.kube/plugins/armada/examples/:/examples quay.io/attcomdev/armada:latest
+  docker run -d --net host -p 8000:8000 --name armada -v ~/.kube/config:/armada/.kube/config -v ~/.bootkube/tls:/.bootkube/tls -v ~/.kube/plugins/armada/examples/:/examples quay.io/attcomdev/armada:latest
   docker exec armada armada "$@"
 fi
 clean_container armada
